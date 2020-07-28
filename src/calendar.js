b8440d1ba57329b594108bfbd45977fbcbd0ad74
fromdafuture <from_da_future@mail.ru>
2020-07-28 13:08:03 +0300

    Start Calendar coding

---
+++ src/calendar.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+
+    let ch = document.getElementById("calendar-holder");
+
+    let str =
+        `   
+<div class="cal-head-holder">
+<div class="cal-left"><</div>
+<div class="cal-right">></div>
+<div class="cal-month">Июль 2020</div>
+
+
+</div>
+
+
+
+
+
+`
+
+    ch.innerHTML = str;
+
+
+}           

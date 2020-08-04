e8bfe3839264c0f8f769422fb538e96d16a4104f
fromdafuture <from_da_future@mail.ru>
2020-08-04 18:17:54 +0300

    Calendar refactor start

--- src/calendar.js
+++ src/calendar.js
@@ -5,87 +5,47 @@ class DateChooser {
         this.monthShown = curDay.getMonth();
         this.firstDay = curDay;
         this.lastDay = curDay;
+
+
+
+
         this.redraw();
     };
 
-
-    createDays(monthDate){
+    init() {
 
     }
 
-
     createMonth(monthDate) {
-        let firstDay = new Date(monthDate.getFullYear(), monthDate.getMonth(), 0);
-
-        let curMonthNumber = monthDate.getMonth();
-        let curMonthDay = monthDate.getDate();
         let curMonthName = monthDate.toLocaleString('ru-RU', { month: 'long' });
         curMonthName = curMonthName[0].toUpperCase() + curMonthName.slice(1);
         let curMonthYear = monthDate.getFullYear();
 
-
-        let lastDay = this.getLastDayOfMonth(monthDate.getYear(), monthDate.getMonth());
-        let lastMonthLastDay = this.getLastDayOfMonth(monthDate.getYear(), monthDate.getMonth() - 1);
-
-        let firstWeekDay = firstDay.getDay() - 1;
-        if (firstWeekDay < 0)
-            firstWeekDay = 6;
-        let lastMonthFirstVisibleDay = -1;
-        if (firstWeekDay > 0)
-            lastMonthFirstVisibleDay = lastMonthLastDay - firstWeekDay;
-
-
         let str = `
-<div class="cal-head-holder">
-    <div class="cal-left"><</div>
-    <div class="cal-right">></div>
-    <div class="cal-month">` + curMonthName + ` ` + curMonthYear + `</div>
-</div>
-<div class="table-holder">
-    <table class="days-table">
-        <tr>
-            <th class="th-cell">Пн</th><th class="th-cell">Вт</th>
-            <th class="th-cell">Ср</th>
-            <th class="th-cell">Чт</th>
-            <th class="th-cell">Пт</th>
-            <th class="th-cell">Сб</th>
-            <th class="th-cell">Вс</th>
-        </tr>`
+        <div class="cal-head-holder">
+            <div class="cal-left"><</div>
+            <div class="cal-right">></div>
+            <div class="cal-month">` + curMonthName + ` ` + curMonthYear + `</div>
+        </div>
+        <div class="table-holder">
+            <table class="days-table">
+                <tr>
+                    <th class="th-cell">Пн</th><th class="th-cell">Вт</th>
+                    <th class="th-cell">Ср</th>
+                    <th class="th-cell">Чт</th>
+                    <th class="th-cell">Пт</th>
+                    <th class="th-cell">Сб</th>
+                    <th class="th-cell">Вс</th>
+                </tr>`
 
+        let curDay = new Date();
+        let datesGenerator = new DatesGenerator(curDay);
+        str += datesGenerator.render(curDay);
 
-        let days_cou = 1
-        let i = 0
-        let row = 0
-        str += `<tr>`
-        if (lastMonthFirstVisibleDay > -1) {
-            for (days_cou = lastMonthFirstVisibleDay; days_cou <= lastMonthLastDay; days_cou++)
-                str += `<td class="td-cell td-cell-grayed"> ` + days_cou + `</td>`
-
-            for (days_cou = 1; days_cou < 7 - (lastMonthLastDay - lastMonthFirstVisibleDay); days_cou++)
-                str += `<td class="td-cell">` + days_cou + `</td>`
-            str += `</tr>`
-            row = 1
-        }
-
-        for (; row++ < 5;) {
-            str += `<tr>`
-            for (i = 0; i < 7; i++) {
-                let closingClassStr = ""
-                if (days_cou == curMonthDay)
-                    closingClassStr += `td-cell-today `
-                if (days_cou > lastDay) {
-                    days_cou = 1;
-                    closingClassStr += `td-cell-grayed `
-                }
-                str += `<td class="td-cell ` + closingClassStr + `">` + days_cou + `</td> `
-                days_cou++;
-            }
-            str += `</tr> `
-        }
-        str += 
-        `</table> 
-        <div> 
-        <div class="cal-clear" >Очистить</div>
+        str +=
+            `</table> 
+            <div> 
+            <div class="cal-clear">Очистить</div>
             <div class="cal-submit">Применить</div>`
         return str;
     }
@@ -96,11 +56,7 @@ class DateChooser {
     };
 
     redraw() {
-
-
-
         let curDay = new Date(2019, 7, 8);
-
         this.calendarHolder.innerHTML = this.createMonth(curDay);
     };
 }
@@ -113,4 +69,116 @@ window.onload = function () {
     let ch = document.getElementById("calendar-holder");
 
     dateChooser = new DateChooser(ch);
-}           
+}
+
+
+class DatesGenerator {
+    constructor(theDate) {
+        this.firstDate = new Date(2020, 7, 2);
+        this.lastDate = new Date(2020, 7, 5);
+        this.days = [];
+        this.genDays(theDate);
+    }
+
+    genDays(date) {
+        this.days.length = 0;
+
+        let curMFirstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay() - 1;
+        if (curMFirstDay < 0) curMFirstDay = 6;
+
+        if (curMFirstDay > 0) {
+            let lastMonthDaysQua = curMFirstDay;
+            let lastMonthFirstDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate() - lastMonthDaysQua + 1;
+            for (let i = 0; i < lastMonthDaysQua; i++) {
+                let newDate = new Date(date.getFullYear(), date.getMonth() - 1, lastMonthFirstDay + i);
+                this.days.push(new Day(newDate, false, this.checkInPeriod(newDate)));
+            }
+        }
+
+        let curMonthDaysQua = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+        for (let i = 1; i <= curMonthDaysQua; i++) {
+            let newDate = new Date(date.getFullYear(), date.getMonth(), i);
+
+            this.days.push(new Day(newDate, true, this.checkInPeriod(newDate)));
+        }
+
+        let curMonthLastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay() - 1;
+        if (curMonthLastDay < 0) curMonthLastDay = 6;
+        if (curMonthLastDay < 6) {
+            for (let i = 1; i < 7 - curMonthLastDay; i++) {
+                let newDate = new Date(date.getFullYear(), date.getMonth() + 1, i);
+
+                this.days.push(new Day(newDate, false, this.checkInPeriod(newDate)));
+            }
+        }
+    }
+
+    checkInPeriod(date) {
+        if (date < this.firstDate || date > this.lastDate)
+            return 0;
+        if (date > this.firstDate && date < this.lastDate)
+            return 3;
+        if (date.getFullYear == this.firstDate.getFullYear && date.getMonth == this.firstDate.getMonth && date.getDate == this.firstDate.getDate)
+            return 1;
+        if (date.getFullYear == this.lastDate.getFullYear && date.getMonth == this.lastDate.getMonth && date.getDate == this.lastDate.getDate)
+            return 2;
+    }
+
+    render() {
+        let str = "";
+        let d = 0;
+        for (let i = 0; i < 6; i++) {
+            str += "<tr>";
+            for (let j = 0; j < 7; j++) {
+                let aDay = this.days[d];
+                str += '<td  class="td-cell ';
+
+                if (!aDay.isInMonth)
+                    str += " td-cell-grayed";
+                if (aDay.isCurrentDay)
+                    str += " td-cell-today";
+                if (aDay.isInPeriod == 1)
+                    str += " td-cell-first";
+                if (aDay.isInPeriod == 2)
+                    str += " td-cell-last";
+                if (aDay.isInPeriod == 3)
+                    str += " td-cell-inside";
+
+                str += '">'
+                str += aDay.dayString;
+                str += "</td>"
+                d++;
+            }
+            str += "</tr>";
+        }
+
+        return str;
+    }
+
+
+}
+
+class Day {
+    constructor(date, isInMonth, isInPeriod) {
+        this.date = date;
+        this.isInMonth = isInMonth;
+        this.isInPeriod = isInPeriod;
+    }
+
+    get isCurrentDay() {
+        const today = new Date()
+        return this.date.getDate() == today.getDate() &&
+            this.date.getMonth() == today.getMonth() &&
+            this.date.getFullYear() == today.getFullYear()
+    }
+
+    get dayString() {
+        return this.date.getDate().toString();
+    }
+    toString() {
+        return this.isCurrentDay ?
+            this.isInMonth ? `<b><u> ${this.date.getDate()} </u> </b>` : this.date.getDate() :
+            this.isInMonth ? `<b> ${this.date.getDate()} </b>` : this.date.getDate();
+    }
+}
